39a75abc35bc5c1d99b57ccd0ef6367e056ce099
Ryo Ota <nwtgck@gmail.com>
2017-11-06 11:47:13 +0900

    [change] Draw a white sphere by sphere ray tracing

--- src/main/scala/SphareRayTracingMain.scala
+++ src/main/scala/SphareRayTracingMain.scala
@@ -16,6 +16,16 @@ object SphareRayTracingMain {
   }
 
   /**
+    * Distance function
+    * @param p
+    * @return
+    */
+  def distanceFunc(p: Vector3D): Float = {
+    val radius: Float = 1.0f
+    p.norm - radius
+  }
+
+  /**
     * Calculate pixel color
     * @param width
     * @param height
@@ -25,13 +35,18 @@ object SphareRayTracingMain {
     */
   def calcPixelColor(width: Int, height: Int, time: Float, fragCoord: Vector2D): Color = {
 
-    // (highly referenced from: https://qiita.com/doxas/items/477fda867da467116f8d)
+    // (highly referenced from: https://wgld.org/d/glsl/g009.html)
+
+    // The number of marching-loop
+    val NMarchingLoop: Int   = 16
+    // Small number for collision detection
+    val Epsilon      : Float = 0.001f
 
     // fragment position
     val p: Vector2D = (fragCoord * 2.0f - Vector2D(width, height)) / Math.min(height, width)
 
     // Camera position
-    val cPos : Vector3D = Vector3D(0.0f, 0.0f, 3.0f)
+    val cPos : Vector3D = Vector3D(0.0f, 0.0f, 2.0f)
     // Camera direction
     val cDir : Vector3D = Vector3D(0.0f, 0.0f, -1.0f)
     // Camera head
@@ -39,11 +54,28 @@ object SphareRayTracingMain {
     // Camera side
     val cSide: Vector3D  = cDir.cross(cUp)
     // Depth of target
-    val targetDepth: Float = 0.1f
+    val targetDepth: Float = 1.0f
 
     // Ray
     val ray: Vector3D = (cSide * p.x + cUp * p.y + cDir * targetDepth).normalize
 
-    Util.makeColor(ray.x, ray.y, -ray.z)
+
+    // Distance between the ray and the nearest object
+    var distance: Float    = 0.0f
+    // Length of current ray
+    var rLen    : Float    = 0.0f
+    // Position of current ray
+    var rPos    : Vector3D = cPos
+    for(i <- 0 until NMarchingLoop){
+      distance = distanceFunc(rPos)
+      rLen += distance
+      rPos = cPos + ray * rLen
+    }
+
+    if(distance.abs < Epsilon){
+      Color.white
+    } else {
+      Color.black
+    }
   }
 }
